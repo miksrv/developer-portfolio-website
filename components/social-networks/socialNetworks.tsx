c713453599d32569c98d37e7caf8c13d33d8518d
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-05-26 08:49:09 -0700

    Fixed code style in the components

--- components/social-networks/socialNetworks.tsx
+++ components/social-networks/socialNetworks.tsx
@@ -6,21 +6,19 @@ import { socialNetworks } from '@/data/socialNetworks'
 
 import styles from './socialNetworks.module.css'
 
-const SocialNetworks: React.FC = () => {
-    return (
-        <div className={styles.social}>
-            {socialNetworks.map((item) => (
-                <a
-                    href={item.link}
-                    title={item.label}
-                    key={item.link}
-                    className={styles.link}
-                >
-                    <SvgIcons name={item.icon} />
-                </a>
-            ))}
-        </div>
-    )
-}
+const SocialNetworks: React.FC = () => (
+    <div className={styles.social}>
+        {socialNetworks.map((item) => (
+            <a
+                href={item.link}
+                title={item.label}
+                key={item.link}
+                className={styles.link}
+            >
+                <SvgIcons name={item.icon} />
+            </a>
+        ))}
+    </div>
+)
 
 export default SocialNetworks
