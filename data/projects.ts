fae8b7180f229a5466f8444919e79b201019132f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2023-06-29 16:27:57 -0700

    Added new project for section

--- data/projects.ts
+++ data/projects.ts
@@ -2,6 +2,7 @@ import { TProject } from '@/types'
 
 import imageGreen from '@/public/project-greenexp.jpg'
 import imageMeteo from '@/public/project-meteostation.jpg'
+import imageSpace from '@/public/project-nearspace.jpg'
 import imageAstro from '@/public/project-observatory.jpg'
 import imageOrion from '@/public/project-orion.jpg'
 
@@ -15,6 +16,11 @@ export const projects: TProject[] = [
         image: imageAstro,
         label: 'Homemade observatory',
         link: 'https://astro.miksoft.pro/'
+    },
+    {
+        image: imageSpace,
+        label: 'Telegram Channel',
+        link: 'https://t.me/+QpMO8yF37DRVPail'
     },
     {
         image: imageGreen,
