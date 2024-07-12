2fc4d77a8fb8239549de12108d14fd2eea59fcde
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-07-12 10:30:38 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,17 +13,17 @@ const App = ({ Component, pageProps }: AppProps) => (
     <>
         <Head>
             <meta
-                name='viewport'
-                content='width=device-width, initial-scale=1, shrink-to-fit=no'
+                name={'viewport'}
+                content={'width=device-width, initial-scale=1, shrink-to-fit=no'}
             />
             <link
-                rel='shortcut icon'
-                href='/favicon.png'
-                key='shortcutIcon'
+                rel={'shortcut icon'}
+                href={'/favicon.png'}
+                key={'shortcutIcon'}
             />
             <link
-                rel='manifest'
-                href='/manifest.json'
+                rel={'manifest'}
+                href={'/manifest.json'}
             />
         </Head>
         <main className={montserrat.className}>
