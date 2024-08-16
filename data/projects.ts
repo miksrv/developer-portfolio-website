00a6b65ced1d3618a72094e687595c9597015094
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-08-16 10:15:07 -0700

    Updated my projects

--- data/projects.ts
+++ data/projects.ts
@@ -1,18 +1,14 @@
-import imageAsteroid from '@/public/project-asteroid.jpg'
-import imageGreen from '@/public/project-greenexp.jpg'
+import imageGeometki from '@/public/project-geometki.jpg'
 import imageMeteo from '@/public/project-meteostation.jpg'
 import imageSpace from '@/public/project-nearspace.jpg'
 import imageAstro from '@/public/project-observatory.jpg'
-import imageOrenburzhie from '@/public/project-orenburzhie.jpg'
-import imageOrion from '@/public/project-orion.jpg'
-import imageRia from '@/public/project-ria56.jpg'
 import { TProject } from '@/types'
 
 export const projects: TProject[] = [
     {
-        image: imageMeteo,
-        label: 'Amateur weather station',
-        link: 'https://meteo.miksoft.pro/'
+        image: imageGeometki,
+        label: 'Interesting places and attractions',
+        link: 'https://geometki.com/'
     },
     {
         image: imageAstro,
@@ -20,33 +16,13 @@ export const projects: TProject[] = [
         link: 'https://astro.miksoft.pro/'
     },
     {
+        image: imageMeteo,
+        label: 'Amateur weather station',
+        link: 'https://meteo.miksoft.pro/'
+    },
+    {
         image: imageSpace,
         label: 'Telegram Channel',
         link: 'https://t.me/+QpMO8yF37DRVPail'
-    },
-    {
-        image: imageGreen,
-        label: 'Greenexp travel project',
-        link: 'https://greenexp.ru'
-    },
-    {
-        image: imageAsteroid,
-        label: 'Asteroids monitoring project',
-        link: 'https://asteroid.miksoft.pro'
-    },
-    {
-        image: imageOrion,
-        label: 'Science projects',
-        link: 'https://vk.com/astrorion'
-    },
-    {
-        image: imageRia,
-        label: 'Orenburg News Portal',
-        link: 'https://ria56.ru'
-    },
-    {
-        image: imageOrenburzhie,
-        label: 'Orenburg News Portal',
-        link: 'https://orenburzhie.ru'
     }
 ]
