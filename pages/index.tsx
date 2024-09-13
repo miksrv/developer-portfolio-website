dc301bbd2e430ba76b113040389ee33eaa2d579a
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-09-13 11:52:08 -0700

    Refactoring pages and images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { NextSeo } from 'next-seo'
 
-import About from '@/components/about/about'
-import Projects from '@/components/projects/projects'
-import Skills from '@/components/skills/skills'
-import SkillsCloud from '@/components/skills-cloud/skillsCloud'
-import SocialNetworks from '@/components/social-networks/socialNetworks'
+import About from '@/components/about'
+import ExternalLinks from '@/components/external-links'
 
-const Page: React.FC = () => (
-    <div className={'wrapperContent'}>
+const MainPage: React.FC = () => (
+    <>
         <NextSeo
-            title='Hi, Im Misha - Programmer, Engineer and Dreamer'
-            description='Hi, my name is Misha and Im a senior software engineer. Welcome to my personal website!'
+            title={'Hi, Im Misha - Programmer, Engineer and Dreamer'}
+            description={'I am a software engineer specializing in frontend and backend development for scalable web applications. With extensive experience in application development, testing, and managing both development teams and projects, I am focused on delivering high-quality and efficient solutions.'}
             openGraph={{
                 images: [
                     {
@@ -24,31 +21,10 @@ const Page: React.FC = () => (
                 siteName: 'miksoft.pro'
             }}
         />
+
         <About />
-        <SocialNetworks />
-        <h2 className={'subTitle'}>What I do</h2>
-        <p className={'subTitleInfo'}>
-            This is a small part of my amateur projects that I continue to support. My hobbies are astronomy and
-            programming and this is perfectly reflected in the following projects:
-        </p>
-        <Projects />
-        <h2 className={'subTitle'}>My Experience</h2>
-        <p className={'subTitleInfo'}>
-            I have over 16 years of experience in creating software for a variety of clients - from government agencies
-            to private companies. Below is a brief overview of my main technical skills and technologies that I use most
-            frequently. I set the level of technology proficiency to be approximate, depending on the number of projects
-            in which this technology was used and how often I use it. The longer I do not use any technology, the lower
-            the level will be.
-        </p>
-        <Skills />
-        <h2 className={'subTitle'}>Technical Skills Cloud</h2>
-        <p className={'subTitleInfo'}>
-            In my work and hobbies, I use not only the skills of a front-end developer, but also those who know the
-            areas of analytics, devops engineering, database architect, web designer, API backend development and much
-            more. Here is the stack of useful technologies I use the most.
-        </p>
-        <SkillsCloud />
-    </div>
+        <ExternalLinks />
+    </>
 )
 
-export default Page
+export default MainPage
---
+++ pages/projects.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { NextSeo } from 'next-seo'
+
+import Projects from '@/components/projects'
+
+const ProjectsPage: React.FC = () => (
+    <>
+        <NextSeo
+            title={'My projects'}
+            description={'This section showcases a selection of my personal projects, which I actively maintain and develop. As a passionate software engineer with a deep interest in both astronomy and programming, these projects reflect the intersection of my hobbies and technical expertise. The majority of these applications are built using modern web technologies like Next.js and React.js, designed to be scalable, efficient, and user-friendly.'}
+            openGraph={{
+                images: [
+                    {
+                        height: 1333,
+                        url: 'https://miksoft.pro/avatar.jpg',
+                        width: 1000
+                    }
+                ],
+                locale: 'en-US',
+                siteName: 'miksoft.pro'
+            }}
+        />
+
+        <section>
+            <h1 className={'pageTitle'}>{'My projects'}</h1>
+            <p>
+                {
+                    'This section showcases a selection of my personal projects, which I actively maintain and develop. As a passionate software engineer with a deep interest in both astronomy and programming, these projects reflect the intersection of my hobbies and technical expertise. The majority of these applications are built using modern web technologies like Next.js and React.js, designed to be scalable, efficient, and user-friendly. Each project represents my commitment to continuous learning and experimentation with new tools and frameworks. Explore the projects below to see how my interests shape my development process.'
+                }
+            </p>
+        </section>
+
+        <Projects />
+    </>
+)
+
+export default ProjectsPage
