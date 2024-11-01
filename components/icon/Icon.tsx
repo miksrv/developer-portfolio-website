9de811db4868663f38d9b689cfa69180c746ada6
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-11-01 10:03:09 -0700

    Added UI tests for Icon and Progress components
    Update Icon.tsx

--- components/icon/Icon.tsx
+++ components/icon/Icon.tsx
@@ -67,7 +67,6 @@ export const Icon: React.FC<IconProps> = ({ name }) => {
                     <path d='M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z' />
                 </svg>
             )
-
     }
 }
 
