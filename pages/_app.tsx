34545779ed7746c27d2397b66d6469f15db190f6
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2024-12-26 15:54:12 -0800

    Fixed theme color for mobile devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,29 +11,47 @@ const App = ({ Component, pageProps }: AppProps) => (
     <>
         <Head>
             <meta
+                name={'mobile-web-app-capable'}
+                content={'yes'}
+            />
+            <meta
                 name={'viewport'}
-                content={'width=device-width, initial-scale=1, shrink-to-fit=no'}
+                content={'width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no'}
+            />
+            <meta
+                name={'apple-mobile-web-app-status-bar-style'}
+                content={'black-translucent'}
+            />
+            <meta
+                name={'theme-color'}
+                content={'#1b1b1b'}
+                media={'(prefers-color-scheme: dark)'}
             />
             <link
-                rel='apple-touch-icon'
-                sizes='180x180'
-                href='/apple-touch-icon.png'
+                rel={'apple-touch-icon'}
+                sizes={'180x180'}
+                href={'/apple-touch-icon.png'}
             />
             <link
-                rel='icon'
-                type='image/png'
-                sizes='32x32'
-                href='/favicon-32x32.png'
+                rel={'icon'}
+                type={'image/png'}
+                sizes={'32x32'}
+                href={'/favicon-32x32.png'}
             />
             <link
-                rel='icon'
-                type='image/png'
-                sizes='16x16'
-                href='/favicon-16x16.png'
+                rel={'icon'}
+                type={'image/png'}
+                sizes={'16x16'}
+                href={'/favicon-16x16.png'}
             />
             <link
-                rel='manifest'
-                href='/site.webmanifest'
+                rel={'icon'}
+                href={'/favicon.ico'}
+                type={'image/x-icon'}
+            />
+            <link
+                rel={'manifest'}
+                href={'/site.webmanifest'}
             />
         </Head>
 
