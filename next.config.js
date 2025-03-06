21e4e859b6fd501998e5d2471d9a47a518156a65
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-03-06 10:40:10 -0800

    Testing mini-css-extract-plugin

--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
 /** @type {import('next').NextConfig} */
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 const nextConfig = {
     output: 'export',
-    // unoptimized - When true, the source image will be served as-is instead of changing quality,
-    // size, or format. Defaults to false.
-    // TODO: first needed to install 'babel-plugin-react-compiler'
-    // experimental: {
-    //     reactCompiler: true,
-    // },
-    // Automatically bundle external packages in the Pages Router:
     bundlePagesRouterDependencies: true,
-    // Opt specific packages out of bundling for both App и Pages Router:
     serverExternalPackages: ['react-activity-calendar', 'react-github-calendar'],
     images: {
         unoptimized: true
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.module.rules.forEach((rule) => {
+                if (rule.oneOf) {
+                    rule.oneOf.forEach((one) => {
+                        if (one.use && one.use.loader === 'next-style-loader') {
+                            one.use.loader = MiniCssExtractPlugin.loader
+                        }
+                    })
+                }
+            })
+
+            config.plugins.push(
+                new MiniCssExtractPlugin({
+                    filename: 'static/css/[name].[contenthash].css'
+                })
+            )
+        }
+
+        return config
     }
 }
 
