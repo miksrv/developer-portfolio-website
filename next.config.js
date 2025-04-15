c04122ccfcd059acb2a8f121a47820ff22e3273b
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2025-04-15 11:13:53 -0700

    Implemented generate sitemap script

--- next.config.js
+++ next.config.js
@@ -7,28 +7,28 @@ const nextConfig = {
     serverExternalPackages: ['react-activity-calendar', 'react-github-calendar'],
     images: {
         unoptimized: true
-    },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.module.rules.forEach((rule) => {
-                if (rule.oneOf) {
-                    rule.oneOf.forEach((one) => {
-                        if (one.use && one.use.loader === 'next-style-loader') {
-                            one.use.loader = MiniCssExtractPlugin.loader
-                        }
-                    })
-                }
-            })
-
-            config.plugins.push(
-                new MiniCssExtractPlugin({
-                    filename: 'static/css/[name].[contenthash].css'
-                })
-            )
-        }
-
-        return config
     }
+    // webpack: (config, { isServer }) => {
+    //     if (!isServer) {
+    //         config.module.rules.forEach((rule) => {
+    //             if (rule.oneOf) {
+    //                 rule.oneOf.forEach((one) => {
+    //                     if (one.use && one.use.loader === 'next-style-loader') {
+    //                         one.use.loader = MiniCssExtractPlugin.loader
+    //                     }
+    //                 })
+    //             }
+    //         })
+    //
+    //         config.plugins.push(
+    //             new MiniCssExtractPlugin({
+    //                 filename: 'static/css/[name].[contenthash].css'
+    //             })
+    //         )
+    //     }
+    //
+    //     return config
+    // }
 }
 
 module.exports = nextConfig
