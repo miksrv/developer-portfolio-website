e0ac6e33299c0be99dcb2abe951cc28497c7d512
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-02-03 12:34:01 -0800

    Updated Experience Skills Page SEO

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -29,7 +29,7 @@ const SkillsPage: React.FC = () => {
 
             <PageTransition>
                 <section>
-                    <h1 className={'pageTitle'}>{'My technical skills'}</h1>
+                    <h1 className={'pageTitle'}>{data?.seo?.skills?.title}</h1>
                     <p>
                         {
                             'With over 19+ years of experience in software development for clients ranging from government agencies to private companies, I’ve built a broad skill set across various technologies. Below is a summary of my key technical skills, ranked by usage frequency and proficiency based on past projects. My expertise spans frontend and backend development, analytics, DevOps, database architecture, web design, and API development, all of which I regularly employ in both professional work and personal projects. Here’s a look at the tech stack I rely on the most.'
