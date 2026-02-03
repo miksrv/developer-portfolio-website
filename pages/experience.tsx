64f4c06fa55f1bee471447e58ad4c728366831eb
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-02-03 13:50:20 -0800

    Updated SEO Projects Page

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -30,17 +30,7 @@ const ProjectsPage: React.FC = () => {
             <PageTransition>
                 <section>
                     <h1 className={'pageTitle'}>{data?.seo?.experience?.title}</h1>
-                    <p>
-                        {
-                            'Over the course of my career as a Fullstack Developer, I’ve had the opportunity to work on a wide range of projects, from scalable web applications to custom backend systems. I specialize in frontend development but have significant experience across the full technology stack. Throughout my roles, I’ve consistently delivered efficient and robust solutions while leading teams, mentoring developers, and collaborating with cross-functional teams to achieve project goals.'
-                        }
-                    </p>
-                    <br />
-                    <p>
-                        {
-                            'My work has spanned various industries, including media, fintech, and e-commerce, where I’ve been responsible for everything from developing new features to optimizing large-scale applications for performance and reliability.'
-                        }
-                    </p>
+                    <p>{data?.seo?.experience?.description}</p>
                 </section>
 
                 <Experience />
