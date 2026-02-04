79e7034967e99f0e7d5acbf55ba75307ba48657f
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-02-04 12:50:11 -0800

    Added animation for SkillsCloud UI Components

--- components/skills-cloud/SkillsCloud.tsx
+++ components/skills-cloud/SkillsCloud.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 import { Backend, DevOps, Frontend, Testing } from '@/data/skills'
 
@@ -13,7 +14,14 @@ export const SkillsCloud: React.FC = () => (
                 ...Object.values(Testing),
                 ...Object.values(DevOps)
             ].map((item, i) => (
-                <li key={`cloud-item-${i}`}>{item}</li>
+                <motion.li
+                    key={`cloud-item-${i}`}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: i * 0.05, duration: 0.2 }}
+                >
+                    {item}
+                </motion.li>
             ))}
         </ul>
     </section>
