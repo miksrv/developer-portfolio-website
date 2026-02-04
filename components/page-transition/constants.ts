4bd1a889292fd07ecf10b634f452102f9fc56d8d
Misha Topchilo <20854498+miksrv@users.noreply.github.com>
2026-02-04 12:15:55 -0800

    Implemented new animation variants for PageTransition

--- components/page-transition/constants.ts
+++ components/page-transition/constants.ts
@@ -1,16 +1,38 @@
 export const parentVariants = {
-    initial: { opacity: 0 },
-    animate: {
-        opacity: 1,
-        transition: {
-            staggerChildren: 0.3,
-            delayChildren: 0.2
-        }
+    fade: {
+        initial: { opacity: 0 },
+        animate: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3,
+                delayChildren: 0.2
+            }
+        },
+        exit: { opacity: 0 }
     },
-    exit: { opacity: 0 }
+    slide: {
+        initial: { x: '-100vw', opacity: 0 },
+        animate: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                stiffness: 50,
+                staggerChildren: 0.2,
+                delayChildren: 0.1
+            }
+        },
+        exit: { x: '100vw', opacity: 0 }
+    }
 }
 
 export const childVariants = {
-    initial: { opacity: 0, y: 50 },
-    animate: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+    fade: {
+        initial: { opacity: 0, y: 50 },
+        animate: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+    },
+    slide: {
+        initial: { opacity: 0, x: 50 },
+        animate: { opacity: 1, x: 0, transition: { duration: 0.5 } }
+    }
 }
